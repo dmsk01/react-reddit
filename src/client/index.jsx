a8390dd80c0d2f91dd89ce17b8c48a2723119a0a
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2022-04-06 22:40:46 +0300

    hot module reload added

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import * as ReactDOM from "react-dom/client";
-import { Header } from "../shared/components/Header/Header";
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+import {Header} from "../shared/Header";
 
-window.addEventListener("load", () => {
-  ReactDOM.hydrateRoot(document.getElementById("root"), <Header />);
-});
+window.addEventListener("load", ()=>{
+  ReactDom.hydrate(<Header/>, document.getElementById('react_root'));
+})
---
+++ src/shared/Header.jsx
@@ -0,0 +1,13 @@
+import { hot } from "react-hot-loader/root";
+import * as React from "react";
+
+function HeaderComponent() {
+  return (
+    <header>
+      <h1>Reddit for our own</h1>
+      <p>Hello there!</p>
+    </header>
+  );
+}
+
+export const Header = hot(HeaderComponent);
