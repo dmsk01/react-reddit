6a24ca6a1bc6747738667b6263d6c460a9f74b07
dmsk01 <53710984+dmsk01@users.noreply.github.com>
2022-09-18 22:45:54 +0300

    add hot posts load

--- src/shared/components/CardsList/CardsList.tsx
+++ src/shared/components/CardsList/CardsList.tsx
@@ -1,17 +1,87 @@
-import React, { useContext } from "react";
-import { postsContext } from "../../context/postsContext";
+import React, { useContext, useEffect, useState } from "react";
+import { IPostsContextData } from "../../context/postsContext";
 import { Card } from "./Card/Card";
+
+import axios from "axios";
+
 import styles from "./cardslist.scss";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store";
+
+export interface IPostData {
+  id?: string;
+  subreddit?: string;
+  title?: string;
+  url?: string;
+  author?: string;
+  created?: number;
+  ups?: number;
+  downs?: number;
+  icon_img?: string;
+  banner_img?: string;
+  selftext?: string;
+  upvote_ratio?: number;
+  thumbnail?: string;
+}
+
+interface IPostItem {
+  data: IPostData;
+}
 
 export function CardsList() {
-  const posts = useContext(postsContext);
-  if (!posts) return null;
+  const token = useSelector<RootState>((state) => state.token);
+  const [posts, setPosts] = useState<IPostItem[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorLoading, setErrorLoading] = useState<string>('');
+
+  useEffect(() => {
+    if (!token) return;
+
+    async function load() {
+      setLoading(true);
+      setErrorLoading('');
+      try {
+        const {
+          data: {
+            data: { children },
+          },
+        } = await axios.get("https://oauth.reddit.com/hot/", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          params: {
+            limit:10
+          }
+        });
+        setPosts(children);
+      } catch (error) {
+        setErrorLoading(String(error));
+        console.error("Failed to load posts in CardList ", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    load();
+  }, [token]);
 
   return (
     <ul className={styles.cardsList}>
-      {posts.map((post) => (
-        <Card key={post?.title} title={post?.title} author={post?.author} url={post?.url} created={post?.created} ups={post?.ups} downs={post?.downs} icon_img={post?.icon_img} banner_img={post?.banner_img} id={post?.id} subreddit={post?.subreddit} selftext={post?.selftext} upvote_ratio={post?.upvote_ratio} />
-      ))}
+      {posts.length === 0 && !loading && !errorLoading && (
+         <li role="status">Sorry, we can't find anything</li>
+      )}
+
+      {posts.map(({ data }) => 
+        (<Card key={data?.title} title={data?.title} author={data?.author} url={data?.url} created={data?.created} ups={data?.ups} downs={data?.downs} icon_img={data?.icon_img} banner_img={data?.thumbnail === 'nsfw' ? '' : data?.thumbnail} id={data?.id} subreddit={data?.subreddit} selftext={data?.selftext} upvote_ratio={data?.upvote_ratio} />)
+      )}
+
+      {errorLoading && (
+        <li role="alert">{errorLoading}</li>
+      )}
+      
+      {loading && (
+        <li role="status">Loading...</li>
+      )}
     </ul>
   );
 }
