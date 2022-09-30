0bf6411db371c44bbf9caf153fac05c4a696b559
dmsk01 <petrov3.14@mail.ru>
2022-09-30 23:05:04 +0300

    add load more btn on each third cardlist load

--- src/shared/components/CardsList/CardsList.tsx
+++ src/shared/components/CardsList/CardsList.tsx
@@ -35,38 +35,55 @@ export function CardsList() {
   const [errorLoading, setErrorLoading] = useState<string>("");
   const [nextAfter, setNextAfter] = useState<string>("");
   const bottomOfListing = useRef<HTMLDivElement>(null);
+  const [isPause, setIsPause] = useState<boolean>(false);
+  const [loadsNumber, setLoadsNumber] = useState<number>(0);
+
+  const handlePause = async () => {
+    setIsPause(false);
+    setLoadsNumber(1);
+    await load();
+  };
+
+  async function load() {
+    setLoading(true);
+    setErrorLoading("");
+    try {
+      const {
+        data: {
+          data: { after, children },
+        },
+      } = await axios.get("https://oauth.reddit.com/hot/", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          limit: 10,
+          after: nextAfter,
+        },
+      });
+      setNextAfter(after);
+      setPosts((prev) => prev.concat(...children));
+      setLoadsNumber((prev) => {
+        return ++prev;
+        // return prev === 2 ? 1 : ++prev;
+      });
+    } catch (error) {
+      setErrorLoading(String(error));
+      console.error("Failed to load posts in CardList ", error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
-    async function load() {
-      setLoading(true);
-      setErrorLoading("");
-      try {
-        const {
-          data: {
-            data: { after, children },
-          },
-        } = await axios.get("https://oauth.reddit.com/hot/", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          params: {
-            limit: 10,
-            after: nextAfter,
-          },
-        });
-        setNextAfter(after);
-        setPosts((prev) => prev.concat(...children));
-      } catch (error) {
-        setErrorLoading(String(error));
-        console.error("Failed to load posts in CardList ", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
     const observer = new IntersectionObserver(
       (entries) => {
-        if (entries[0].isIntersecting) load();
+        console.log(loadsNumber);
+        if (loadsNumber === 2) setIsPause(true);
+
+        if (entries[0].isIntersecting && loadsNumber !== 2) {
+          load();
+        }
       },
       {
         rootMargin: "30px",
@@ -80,18 +97,43 @@ export function CardsList() {
   }, [bottomOfListing.current, nextAfter, token]);
 
   return (
-    <ul className={styles.cardsList}>
-      {posts.length === 0 && !loading && !errorLoading && <li role="status">Sorry, we can't find anything</li>}
+    <>
+      <ul className={styles.cardsList}>
+        {posts.length === 0 && !loading && !errorLoading && (
+          <li role="status">Sorry, we can't find anything</li>
+        )}
 
-      {posts.map(({ data }) => (
-        <Card key={data?.title} title={data?.title} author={data?.author} url={data?.url} created={data?.created} ups={data?.ups} downs={data?.downs} icon_img={data?.icon_img} banner_img={data?.thumbnail === "nsfw" ? "" : data?.thumbnail} id={data?.id} subreddit={data?.subreddit} selftext={data?.selftext} upvote_ratio={data?.upvote_ratio} />
-      ))}
+        {posts.map(({ data }) => (
+          <Card
+            key={data?.title}
+            title={data?.title}
+            author={data?.author}
+            url={data?.url}
+            created={data?.created}
+            ups={data?.ups}
+            downs={data?.downs}
+            icon_img={data?.icon_img}
+            banner_img={data?.thumbnail === "nsfw" ? "" : data?.thumbnail}
+            id={data?.id}
+            subreddit={data?.subreddit}
+            selftext={data?.selftext}
+            upvote_ratio={data?.upvote_ratio}
+          />
+        ))}
 
-      <div ref={bottomOfListing} />
+        <div ref={bottomOfListing} />
 
-      {errorLoading && <li role="alert">{errorLoading}</li>}
+        {errorLoading && <li role="alert">{errorLoading}</li>}
 
-      {loading && <li role="status">Loading...</li>}
-    </ul>
+        {loading && <li role="status">Loading...</li>}
+      </ul>
+      {isPause && (
+        <div role="status">
+          <button type="button" className="button" onClick={handlePause}>
+            Load more
+          </button>
+        </div>
+      )}
+    </>
   );
 }
